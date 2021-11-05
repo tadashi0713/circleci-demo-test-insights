94a0328ba624a07e3a12de345d4d99fea1594aad
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-11-05 15:17:32 +0900

    test with 1 min

--- demo.test.ts
+++ demo.test.ts
@@ -1,5 +1,12 @@
+const { setTimeout: sleep } = require('timers/promises');
+
 describe('Test Insights Demo', () => {
   it('100% Success', () => {
+    expect(true).toEqual(true)
+  })
+
+  it('Takes 1 min', async () => {
+    await sleep(60000)
     expect(true).toEqual(true)
   })
 });
