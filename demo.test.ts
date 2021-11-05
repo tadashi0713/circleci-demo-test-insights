a3706f5926dab32160726e7707ef9fd93f5b4b64
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-11-05 15:25:51 +0900

    Add 50% test

--- demo.test.ts
+++ demo.test.ts
@@ -5,6 +5,14 @@ describe('Test Insights Demo', () => {
     expect(true).toEqual(true)
   })
 
+  it('50% Success', () => {
+    if(Math.random() < 0.5) {
+      expect(true).toEqual(true)
+    } else {
+      expect(false).toEqual(true)
+    }
+  })
+
   it('Takes 1 min', async () => {
     await sleep(60000)
     expect(true).toEqual(true)
